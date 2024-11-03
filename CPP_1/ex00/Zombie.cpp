caa6e7a27f68ca248a8335fed8cd729ed3e4e7f3
ochetrit <oscarchetrit@gmail.com>
2024-11-03 21:54:07 +0100

    bonjour

---
+++ CPP_1/ex00/Zombie.cpp
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   init_data.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ochetrit <ochetrit@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/16 16:43:46 by ochetrit          #+#    #+#             */
+/*   Updated: 2024/09/17 15:06:48 by ochetrit         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Zombie.hpp"
+
+Zombie::Zombie(std::string name)
+{
+	this->name = name;
+	std::cout << this->name << "just stepped on ur lawn" << std::endl;
+}
+
+Zombie::~Zombie( void )
+{
+	std::cout << this->name << " just died from your defense" << std::endl;
+}
+
+void	Zombie::announce( void )
+{
+	std::cout << this->name << " BraiiiiiiinnnzzzZ..." << std::endl;
+}
---
+++ CPP_1/ex00/Zombie.hpp
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   init_data.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ochetrit <ochetrit@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/16 16:43:46 by ochetrit          #+#    #+#             */
+/*   Updated: 2024/09/17 15:06:48 by ochetrit         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef ZOMBIE_HPP
+# define ZOMBIE_HPP
+# include <string>
+# include <iostream>
+
+
+Class Zombie
+{
+	private:
+		std::string name;
+	public:
+		Zombie(std::string name, std::string type);
+		~Zombie(void);
+		void announce(void);
+}
+
+#endif
